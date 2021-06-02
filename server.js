cfb6dbc81cbfc0b8b8e51cf7331e7337d02136ab
Vinod Gupta <vinodg@MITR-C07NL12MG1HW.local>
2021-06-02 21:11:51 +0530

    created graphql schema and added json server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+  }),
+);
+
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
+});
